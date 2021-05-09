e677b3230885060ddd0f092bf0b336a8fcc7c79c
GhanatheyOmkaar123 <71542551+GhanatheyOmkaar123@users.noreply.github.com>
2021-05-09 17:56:48 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 300,200)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 300,220)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("CUTNIFON",1200,350);
+    fill("lightblue")
+    text("Hint: Perfoms a particular task !!",1200,370);
+
+    fill("white")
+    textSize(15)
+    text("ATADASBA",300,550);
+    fill("lightblue")
+    text("Hint: Stores all information !!",300,570);
+
+}
